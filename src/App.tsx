229ffbf94a6d160d9a739d0d6f0edaa69f027814
Julien Bonte <julien.bonte@keepeek.com>
2021-03-02 23:11:38 +0100

    install eslint

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,20 @@ import './App.css';
 import p5Types from "p5"; //Import this for typechecking and intellisense
 
 import Sketch from "react-p5";
-import { useEffect, useState } from 'react';
-
 
 function App() {
-  const [x, setX] = useState(1);
-	const y = 50;
 
   const setup = (p5: p5Types, canvasParentRef: Element) => {
     p5.colorMode('hsb', 360, 100, 100, 1);
     p5.createCanvas(window.innerWidth, window.innerHeight).parent(canvasParentRef);
+    for(let i = 0; i < 100; i += 1) {
+      
+    }
   }
 
   const draw = (p5: p5Types) => {
-		p5.background(0);
-		p5.ellipse(x, y, 50, 50);
-		setX(x+2);
-	};
 
-  useEffect(() => {
-    if(x > window.innerWidth) {
-      setX(0)
-    }
-  }, [x])
+	};
 
   return (
     <Sketch className="wall" setup={setup} draw={draw}/>
