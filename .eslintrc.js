c02cffa8ba5d01dc531f7f47b7593866807dd97e
Julien Bonte <julien.bonte@keepeek.com>
2021-03-02 23:22:49 +0100

    add rulez and lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
+      legacyDecorators: true,
       jsx: true,
     },
     ecmaVersion: 12,
@@ -19,6 +20,46 @@ module.exports = {
     'react',
     '@typescript-eslint',
   ],
+  settings: {
+    react: {
+      version: '17',
+    },
+  },
   rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.ts', '.tsx'] }],
+    'no-use-before-define': 0,
+    'prefer-template': 'error',
+    'space-before-function-paren': 0,
+    'react/react-in-jsx-scope': 0,
+    'react/prop-types': 0,
+    'react/jsx-handler-names': 0,
+    'react/jsx-fragments': 0,
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'function-expression',
+      },
+    ],
+    'react/no-unused-prop-types': 0,
+    'import/export': 0,
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', ['parent', 'sibling']],
+        pathGroups: [
+          {
+            pattern: 'react',
+            group: 'external',
+            position: 'before',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['react'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
   },
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
+import React, { FC } from 'react';
+
 import './App.css';
-import p5Types from "p5"; //Import this for typechecking and intellisense
+import p5Types from 'p5'; // Import this for typechecking and intellisense
+import Sketch from 'react-p5';
 
-import Sketch from "react-p5";
-
-function App() {
-
+const App:FC = function() {
   const setup = (p5: p5Types, canvasParentRef: Element) => {
     p5.colorMode('hsb', 360, 100, 100, 1);
     p5.createCanvas(window.innerWidth, window.innerHeight).parent(canvasParentRef);
-    for(let i = 0; i < 100; i += 1) {
-      
+    for (let i = 0; i < 100; i += 1) {
+
     }
-  }
+  };
 
   const draw = (p5: p5Types) => {
 
-	};
+  };
 
   return (
-    <Sketch className="wall" setup={setup} draw={draw}/>
+    <Sketch className="wall" setup={setup} draw={draw} />
   );
-}
+};
 
 export default App;
