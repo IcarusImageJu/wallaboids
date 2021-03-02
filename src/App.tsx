8d78dc639f8048158ba493914012106ae33e1022
Julien Bonte <julien.bonte@keepeek.com>
2021-03-02 23:02:04 +0100

    init p5

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import p5Types from "p5"; //Import this for typechecking and intellisense
+
+import Sketch from "react-p5";
+import { useEffect, useState } from 'react';
+
 
 function App() {
+  const [x, setX] = useState(1);
+	const y = 50;
+
+  const setup = (p5: p5Types, canvasParentRef: Element) => {
+    p5.colorMode('hsb', 360, 100, 100, 1);
+    p5.createCanvas(window.innerWidth, window.innerHeight).parent(canvasParentRef);
+  }
+
+  const draw = (p5: p5Types) => {
+		p5.background(0);
+		p5.ellipse(x, y, 50, 50);
+		setX(x+2);
+	};
+
+  useEffect(() => {
+    if(x > window.innerWidth) {
+      setX(0)
+    }
+  }, [x])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Sketch className="wall" setup={setup} draw={draw}/>
   );
 }
 
